5887510a7469e437df9f998529c45a64f1280069
xuezumin <xuezumin@shuwen.com>
2022-03-10 12:58:04 +0800

    脚手架进行最简单化的修改

---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+// 创建并暴露组件
+export default class Hello extends Component {
+    render() {
+        return (
+            <h1>Hello react!</h1>
+        );
+    }
+
+}
